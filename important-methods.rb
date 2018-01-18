b5398781ac166352ac1a30fa23bb28dd8b042c02
Arun Kumar Kandhasamy <k.arun@outlook.com>
2018-01-18 10:51:19 +0530

    important method added

--- important-methods.rb
+++ important-methods.rb
@@ -7,3 +7,11 @@ puts [1, 2, 3].respond_to?(:to_sym)
 # We can pass a Ruby method name around with a symbol?
 strings = ["1", "2", "3"]
 nums = strings.map(&:to_i)
+
+#creates an array of symbols easily
+array_of_symbols = %i{foo bar} 
+puts array_of_symbols
+
+#this also creates an array of symbols, but in a unnecessary way
+array_of_symbols_1 = %w( it is less elegant to create arrays of symbols ).map( &:to_sym )
+puts array_of_symbols_1
